aa7bf56a1a99c81652f0a266204019a3689dbc37
Ali ;) <cliali1181@gmail.com>
2024-12-09 23:42:30 +0330

    fix(static): fix static load problem

--- {{cookiecutter.project_name}}/config/settings/compressor.py
+++ {{cookiecutter.project_name}}/config/settings/compressor.py
@@ -6,4 +6,8 @@ COMPRESS_ROOT = os.path.join(BASE_DIR, "static")
 
 COMPRESS_ENABLED = True
 
-STATICFILES_FINDERS = ("compressor.finders.CompressorFinder",)
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "compressor.finders.CompressorFinder",
+)
